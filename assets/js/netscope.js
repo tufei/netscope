fac3257814599bf93b1ca1167bebb4f8826c4b84
Yufei Yuan <yfyuan@gmail.com>
2018-09-22 17:19:09 -0700

    Updated the assets for Split layer.

--- assets/js/netscope.js
+++ assets/js/netscope.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var AppController, Editor, Notify, Renderer,
   bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
   slice = [].slice;
@@ -1087,6 +1087,36 @@ layers.Crop = this.CropLayer = (function() {
   };
 
   return CropLayer;
+
+})();
+
+layers.Split = this.SplitLayer = (function() {
+  function SplitLayer() {
+    this.checkParameters = bind(this.checkParameters, this);
+    this.inferShapes = bind(this.inferShapes, this);
+  }
+
+  SplitLayer.prototype.inferShapes = function(bottoms, tops) {
+    var i, j, outputShape, ref, results;
+    if ((tops != null ? tops[0] : void 0) == null) {
+      return;
+    }
+    this.checkParameters(bottoms, tops);
+    outputShape = bottoms[0].shape.slice(0);
+    results = [];
+    for (i = j = 0, ref = tops.length; 0 <= ref ? j < ref : j > ref; i = 0 <= ref ? ++j : --j) {
+      results.push(tops[i].shape = outputShape);
+    }
+    return results;
+  };
+
+  SplitLayer.prototype.checkParameters = function(bottoms, tops) {
+    if ((bottoms != null ? bottoms.length : void 0) !== 1) {
+      throw 'Split layer must have exactly one bottom blob.';
+    }
+  };
+
+  return SplitLayer;
 
 })();
 
